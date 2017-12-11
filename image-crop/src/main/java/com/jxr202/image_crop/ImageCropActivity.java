0663a07d71a13adab34496db3b2fb0b567e82425
jxr202 <jxr202@foxmail.com>
2017-12-11 15:46:52 +0800

    【蒋先荣】：优化了相机权限相关的提示，修复因权限而崩溃的问题

--- image-crop/src/main/java/com/jxr202/image_crop/ImageCropActivity.java
+++ image-crop/src/main/java/com/jxr202/image_crop/ImageCropActivity.java
@@ -1,11 +1,21 @@
 package com.jxr202.image_crop;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -18,22 +28,48 @@ import java.io.File;
 /**
  * Created by jxr202 on 2017/11/29
  */
-public class ImageCropActivity extends Activity implements View.OnClickListener{
+public class ImageCropActivity extends Activity implements View.OnClickListener {
 
 
     private static final String TAG = "jxr";
-    /** 头像文件 */
+    /**
+     * 头像文件
+     */
     private static final String IMAGE_FILE_NAME = "temp_head_image.jpg";
-    /** 请求相册 */
+    /**
+     * 请求相册
+     */
     private static final int CODE_GALLERY_REQUEST = 0xa0;
-    /** 请求拍照 **/
+    /**
+     * 请求拍照
+     **/
     private static final int CODE_CAMERA_REQUEST = 0xa1;
-    /** 裁剪返回 **/
+    /**
+     * 裁剪返回
+     **/
     private static final int CODE_RESULT_REQUEST = 0xa2;
-    /** 裁剪后图片的宽(X)和高(Y) */
+    /**
+     * 请求相机权限
+     **/
+    private static final int CODE_REQUEST_PERMISSIONS = 0xa3;
+    /**
+     * 再次请求相机权限
+     **/
+    private static final int CODE_REQUEST_SETTING = 0xa4;
+
+    /**
+     * 裁剪后图片的宽(X)和高(Y)
+     */
     private int outputX, outputY;
-    /** 裁剪比例 */
+    /**
+     * 裁剪比例
+     */
     private int aspectX, aspectY;
+    /**
+     * 相机权限
+     */
+    private String[] permissions = {Manifest.permission.CAMERA};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,27 +109,133 @@ public class ImageCropActivity extends Activity implements View.OnClickListener{
      * 从本地相册选取图片作为头像
      */
     private void choseHeadImageFromGallery() {
-        Intent intentFromGallery = new Intent();
-        // 设置文件类型
-        intentFromGallery.setType("image/*");//选择图片
+
+        // ACTION_GET_CONTENT 让用户自己选择使用哪里的图片，如最近、下载或是相册等等
+        /*Intent intentFromGallery = new Intent();
         intentFromGallery.setAction(Intent.ACTION_GET_CONTENT);
-        //你需要使用系统提供的startActivityForResult(Intent intent,int requestCode)方法打开新的Activity
-        startActivityForResult(intentFromGallery, CODE_GALLERY_REQUEST);
+        intentFromGallery.setType("image*//*");//选择图片*/
+
+        // ACTION_PICK 则是直接使用相册中的图片
+        Intent intentPhoto = new Intent(Intent.ACTION_PICK, null);
+        intentPhoto.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+
+        // 发起选图请求
+        startActivityForResult(intentPhoto, CODE_GALLERY_REQUEST);
     }
 
     /**
      * 启动手机相机拍摄照片作为头像
      */
     private void choseHeadImageFromCameraCapture() {
-        Intent intentFromCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        // 判断存储卡是否可用，存储照片文件
-        if (hasSdcard()) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+            if (i != PackageManager.PERMISSION_GRANTED) {
+                showRequestPermissionDialog();
+            } else {
+                startCamera();
+            }
+        } else {
+            startCamera();
+        }
+    }
+
+    /**
+     * 开始拍照
+     */
+    private void startCamera() {
+        Intent intentFromCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (hasSdcard()) {  // 判断存储卡是否可用，存储照片文件
             intentFromCapture.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME)));
         }
-
         startActivityForResult(intentFromCapture, CODE_CAMERA_REQUEST);
     }
+
+    /**
+     * HeroBand拍照方式
+     */
+    private void startCameraByHero() {
+        File file = new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME);
+        //Android N :FileUriExposedException solved
+        ContentValues contentValues = new ContentValues(1);
+        contentValues.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+        Uri uri1 = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri1);
+        startActivityForResult(intent, CODE_CAMERA_REQUEST);
+    }
+
+
+    /**
+     * 提示用户该请求权限的弹出框
+     */
+    private void showRequestPermissionDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.camera_available_title)
+                .setMessage(R.string.camera_available_message)
+                .setPositiveButton(R.string.open_right_now, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(ImageCropActivity.this, permissions, CODE_REQUEST_PERMISSIONS);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setResult(RESULT_CANCELED, null);
+                        finish();
+                    }
+                }).setCancelable(false).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == CODE_REQUEST_PERMISSIONS) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    // 判断用户是否 点击了不再提醒。(检测该权限是否还可以申请)
+                    if (!shouldShowRequestPermissionRationale(permissions[0])) {
+                        showSettingPermissionDialog();
+                    } else {
+                        setResult(RESULT_CANCELED, null);
+                        finish();
+                    }
+                } else {
+                    startCamera();
+                }
+            }
+        }
+    }
+
+    /**
+     * 再次警告用户权限框
+     */
+    private void showSettingPermissionDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.camera_available_title)
+                .setMessage(R.string.camera_available_message)
+                .setPositiveButton(R.string.open_right_now, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        goToSetting();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .setCancelable(false).show();
+    }
+
+    /**
+     * 跳转到当前应用的设置界面
+     */
+    private void goToSetting() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, CODE_REQUEST_SETTING);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -101,8 +243,8 @@ public class ImageCropActivity extends Activity implements View.OnClickListener{
         Log.i(TAG, "onActivityResult -> requestCode: " + requestCode + ", resultCode: " + resultCode + ", intent: " + intent);
 
         // 用户没有进行有效的设置操作，返回
-        if (resultCode == RESULT_CANCELED) {//取消
-            Toast.makeText(getApplication(), "取消", Toast.LENGTH_LONG).show();
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(getApplication(), R.string.cancel, Toast.LENGTH_LONG).show();
             setResult(RESULT_CANCELED, intent);
             finish();
             return;
@@ -133,6 +275,17 @@ public class ImageCropActivity extends Activity implements View.OnClickListener{
                 }
                 break;
             }
+            case CODE_REQUEST_SETTING: {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+                    if (i != PackageManager.PERMISSION_GRANTED) {
+                        showSettingPermissionDialog();
+                    } else {
+                        startCamera();
+                    }
+                }
+                break;
+            }
         }
 
         super.onActivityResult(requestCode, resultCode, intent);
@@ -142,25 +295,16 @@ public class ImageCropActivity extends Activity implements View.OnClickListener{
      * 裁剪原始的图片
      */
     public void cropRawPhoto(Uri uri) {
-
         Intent intent = new Intent("com.android.camera.action.CROP");
-
         intent.setDataAndType(uri, "image/*");
-
-        //把裁剪的数据填入里面
-
-        // 设置裁剪
         intent.putExtra("crop", "true");
-
         // aspectX , aspectY :宽高的比例
         intent.putExtra("aspectX", aspectX);
         intent.putExtra("aspectY", aspectY);
-
         // outputX , outputY : 裁剪图片宽高
         intent.putExtra("outputX", outputX);
         intent.putExtra("outputY", outputY);
         intent.putExtra("return-data", true);
-
         startActivityForResult(intent, CODE_RESULT_REQUEST);
     }
 
@@ -169,7 +313,6 @@ public class ImageCropActivity extends Activity implements View.OnClickListener{
      * 检查设备是否存在SDCard的工具方法
      */
     public static boolean hasSdcard() {
-
         return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
     }
 
@@ -181,7 +324,7 @@ public class ImageCropActivity extends Activity implements View.OnClickListener{
         } else if (i == R.id.choosePhoto) {
             choseHeadImageFromGallery();
         } else if (i == R.id.cancelPhoto) {
-            Toast.makeText(getApplication(), "取消", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplication(), R.string.cancel, Toast.LENGTH_LONG).show();
             setResult(RESULT_CANCELED, null);
             finish();
         }
